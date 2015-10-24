dd9a92a19f092f6dee42eacd1fb443f76a8459a6
nmusgrave <naomi.g.musgrave@gmail.com>
2015-10-24 16:17:03 -0400

    adding placeholders for documentation

--- visualizer.js
+++ visualizer.js
@@ -11,37 +11,3 @@ sampleSVG.append("circle")
     .attr("cy", 50)
     .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
     .on("mouseout", function(){d3.select(this).style("fill", "white");});
-
-d3.csv("fourPeople_EnergyProfiles.csv", function(data) {
-    // the columns you'd like to display
-    var columns = ["name", "age"];
-
-    var table = d3.select("#energy_profile_table").append("table"),
-        thead = table.append("thead"),
-        tbody = table.append("tbody");
-
-    // append the header row
-    thead.append("tr")
-        .selectAll("th")
-        .data(columns)
-        .enter()
-        .append("th")
-            .text(function(column) { return column; });
-
-    // create a row for each object in the data
-    var rows = tbody.selectAll("tr")
-        .data(data)
-        .enter()
-        .append("tr");
-
-    // create a cell in each row for each column
-    var cells = rows.selectAll("td")
-        .data(function(row) {
-            return columns.map(function(column) {
-                return {column: column, value: row[column]};
-            });
-        })
-        .enter()
-        .append("td")
-            .text(function(d) { return d.value; });
-});
