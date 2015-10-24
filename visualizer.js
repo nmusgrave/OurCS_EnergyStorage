23bf188c992290407d017b20f2684d9727e63e56
nmusgrave <naomi.g.musgrave@gmail.com>
2015-10-24 15:21:10 -0400

    set up server to be able to access csv w/o CORS erros.

---
+++ visualizer.js
@@ -0,0 +1,25 @@
+var sampleSVG = d3.select("#viz")
+    .append("svg")
+    .attr("width", 100)
+    .attr("height", 100);    
+
+sampleSVG.append("circle")
+    .style("stroke", "gray")
+    .style("fill", "white")
+    .attr("r", 40)
+    .attr("cx", 50)
+    .attr("cy", 50)
+    .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+    .on("mouseout", function(){d3.select(this).style("fill", "white");});
+
+d3.csv("foo.csv", function(d) {
+    console.log(d);
+  return {
+    year: new Date(+d.Year, 0, 1), // convert "Year" column to Date
+    make: d.Make,
+    model: d.Model,
+    length: +d.Length // convert "Length" column to number
+  };
+}, function(error, rows) {
+  console.log(rows);
+});
